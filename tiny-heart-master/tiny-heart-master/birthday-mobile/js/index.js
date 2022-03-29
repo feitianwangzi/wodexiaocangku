80a6f13c6d956bca55bd4c812a15dcdad561ca9d
feitianwangzi <2689346005@qq.com>
2022-03-29 13:55:54 +0800

    第二次修改生日

--- tiny-heart-master/tiny-heart-master/birthday-mobile/js/index.js
+++ tiny-heart-master/tiny-heart-master/birthday-mobile/js/index.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="123" &&  pwd=="123"){
+    if(userName=="张子怡" &&  pwd=="20010308"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
